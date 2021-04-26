6f9a795154278c3c5596c5e3ed1d29aaa8111621
Hubert Kowalski <hubert160491@gmail.com>
2021-04-26 19:57:09 +0200

    Stworzenie projektu

---
+++ src/test/java/pl/hk/zadanie_25/Zadanie25ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hk.zadanie_25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
